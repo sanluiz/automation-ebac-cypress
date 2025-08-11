93c610f88fce4310d75975f2428368cb2452fd15
Luiz Santos <luiz.snto@outlook.com>
2025-08-11 18:22:40 -0300

    test(login): incluir cenários de login válido e inválido

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,41 @@
+/// <reference types="cypress" />
+
+context('Funcionalidade: Login', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+
+    });
+
+
+    describe('Login com sucesso', () => {
+        it('Deve logar com sucesso', () => {
+            cy.get('#username').type('aluno_ebac@teste.com')
+            cy.get('#password').type('teste@teste.com')
+            cy.get('.woocommerce-form > .button').click()
+
+            cy.get('a > .hidden-xs').should('contain', 'Welcome Aline !')
+        })
+    })
+
+
+    describe('Login com falha', () => {
+        it('Não deve logar com email invalido', () => {
+            cy.get('#username').type('email_invalido@teste.com')
+            cy.get('#password').type('teste@teste.com')
+            cy.get('.woocommerce-form > .button').click()
+
+            cy.get('.woocommerce-error > li').should('contain', 'Endereço de e-mail desconhecido.')
+        })
+
+        it('Deve exibir mensagem de erro ao inserir senha invalidos', () => {
+            cy.get('#username').type('aluno_ebac@teste.com')
+            cy.get('#password').type('senha_invalida.com')
+            cy.get('.woocommerce-form > .button').click()
+
+            cy.get('.woocommerce-error > li').should('contain', 'Erro: A senha fornecida para o e-mail aluno_ebac@teste.com está incorreta.')
+        })
+
+    })
+
+})
