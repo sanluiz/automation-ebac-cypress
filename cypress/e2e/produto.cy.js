2a3be4cad0d7bfd8954297c80a86e5e0e8cc7a47
Luiz Santos <luiz.snto@outlook.com>
2025-08-14 10:47:23 -0300

    test(produtos): incluir cenário de produto no carrinho

---
+++ cypress/e2e/produto.cy.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+context('Funcionalidade: Prodto', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar produto', () => {
+        var qtd = 5
+
+        cy.get('.product-block').first().click()
+        cy.get('.button-variable-item-L').click()
+        cy.get('.button-variable-item-Blue').click()
+        cy.get('.input-text').clear().type(5)
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.get('.woocommerce-message').should('contain', qtd + ' × “Abominable Hoodie”')
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', qtd)
+
+    })
+
+})
